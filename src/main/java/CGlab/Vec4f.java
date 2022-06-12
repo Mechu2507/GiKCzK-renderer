1503fd734f925bb7d5c956023a0a15e988bba8ca
Mechu2507 <mechu2507@gmail.com>
2022-06-12 19:46:06 +0200

    Lab.5 Zad.2

--- src/main/java/CGlab/Vec4f.java
+++ src/main/java/CGlab/Vec4f.java
@@ -1,4 +1,39 @@
 package CGlab;
 
 public class Vec4f {
+    public float x;
+
+    public Vec4f(float x, float y, float z, float w) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = w;
+    }
+
+    public float y;
+    public float z;
+    public float w;
+
+    @Override
+    public String toString() {
+        return x + " " + y + " " + z + " " + w;
+    }
+
+    public float x() {
+        return x;
+    }
+
+    public float w() {
+        return w;
+    }
+
+    public float y() {
+        return y;
+    }
+
+    public float z() {
+        return z;
+    }
+
+
 }
